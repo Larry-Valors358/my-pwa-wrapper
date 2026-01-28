c2a0b8f375007074e815dae77efb189a0a2a2a93
ValorsMan <valo.seika2025@gmail.com>
2026-01-28 11:58:02 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+/**
+ * アプリケーション設定ファイル
+ * GASのURLを更新した時は、GAS_URLを更新する。
+ */
+const APP_CONFIG = {
+    GAS_URL: "https://script.google.com/macros/s/AKfycbwXods9Lda8UwtUh4YfWfazZY3gLkn9vWsmOAkLc_SD1oXIf-1Js3zQpT7Y5Tp7dHYJAQ/exec",
+    
+    APP_TITLE: "戦術対抗戦ログ"
+  
+    // 他にも設定を増やしたくなった時にここに追加
+};
