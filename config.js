c15e4bba0985a2c0c17a5ac57c614fa4a185c23c
ValorsMan <valo.seika2025@gmail.com>
2026-02-21 12:22:57 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,7 @@
  * GASのURLを更新した時は、GAS_URLを更新する。
  */
 const APP_CONFIG = {
-    GAS_URL: "https://script.google.com/macros/s/AKfycbwXods9Lda8UwtUh4YfWfazZY3gLkn9vWsmOAkLc_SD1oXIf-1Js3zQpT7Y5Tp7dHYJAQ/exec",
-    
+    GAS_URL: "https://script.google.com/macros/s/AKfycbzZgt0--Vrg1SxjNyXTfTlACAH6Xn3seEgpRnCW4DpbzRxaktvHyRlK9bnpPt_EuKaw/exec",
     APP_TITLE: "戦術対抗戦ログ"
   
     // 他にも設定を増やしたくなった時にここに追加
